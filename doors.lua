88340df2585a28a0703e21c13922b89a541e1743
FaceDeer <derksenmobile@gmail.com>
2017-09-26 19:18:54 -0600

    jaildoor recipe fix

--- doors.lua
+++ doors.lua
@@ -16,6 +16,21 @@ if minetest.get_modpath("doors") then
 			{"default:tree", "default:tree"},
 		}
 	})
+
+	local door_recipe
+	if minetest.get_modpath("xpanes") then
+		door_recipe = {
+			{"xpanes:jailbars_flat", "xpanes:jailbars_flat"},
+			{"xpanes:jailbars_flat", "xpanes:jailbars_flat"},
+			{"xpanes:jailbars_flat", "xpanes:jailbars_flat"},
+		}
+	else
+		door_recipe = {
+			{"default:steel_ingot", ""},
+			{"", "default:steel_ingot"},
+			{"default:steel_ingot", ""},
+		}
+	end
 	
 	doors.register("castle_gates:jail_door", {
 		tiles = {{ name = "castle_door_jail.png", backface_culling = true }},
@@ -25,11 +40,7 @@ if minetest.get_modpath("doors") then
 		groups = { cracky = 2, door = 1, flow_through = 1},
 		sound_open = "doors_steel_door_open",
 		sound_close = "doors_steel_door_close",
-		recipe = {
-			{"castle_gates:jailbars", "castle_gates:jailbars"},
-			{"castle_gates:jailbars", "castle_gates:jailbars"},
-			{"castle_gates:jailbars", "castle_gates:jailbars"},
-		}
+		recipe = door_recipe,
 	})
 	
 	minetest.register_alias("castle:oak_door_a", "castle_gates:oak_door_a")
